455daa6653dd972e587b3c39b4f301a15d2aee65
Hiro Kato <hirokikato1@gmail.com>
2023-07-19 22:36:29 -0400

    updates advanced lab challenges

--- src/index.js
+++ src/index.js
@@ -1,75 +1,126 @@
 // write your code here
 //see all ramen images in the div with the id of ramen-menu
-    //1. request the data from the server to get all ramen objects
-    //2. display image for each ramen in img tag inside #ramen-menu div
+//1. request the data from the server to get all ramen objects
+//2. display image for each ramen in img tag inside #ramen-menu div
 //Add event listener to each image in #ramen-menu div when clicked
-    //1. Render all info about ramen inside #ramen-detail div
-    //2. Rendered info should reflect comments and rating?
+//1. Render all info about ramen inside #ramen-detail div
+//2. Rendered info should reflect comments and rating?
 //Create a new ramen after submitting new-ramen form
-    //1. Prevent form default
-    //2. Add event listener to create button
-    //3. No post request because ramen does not persist... the browser eats it lol?
+//1. Prevent form default
+//2. Add event listener to create button
+//3. No post request because ramen does not persist... the browser eats it lol?
 
-
-const ramenMenu = document.getElementById("ramen-menu")
-const ramenDetail = document.getElementById("ramen-detail")
-
-
-//getRamenObjects from API
+const ramenMenu = document.getElementById("ramen-menu");
+const ramenDetail = document.getElementById("ramen-detail");
 const url = "http://localhost:3000/ramens";
-fetch(url)
-.then(res => {return res.json()})
-.then(data => {
-    for(let ramen in data){
-        const ramenName = data[ramen].name
-        const restaurantName = data[ramen].restaurant
-        const imageUrl = data[ramen].image
-        const ratingInfo = data[ramen].rating
-        const ramenComment = data[ramen].comment
+let displayedRamen = null
 
-        //Renders ramen images in div #ramen-menu
-        const imgElement = document.createElement('img')
-        imgElement.src = imageUrl
-        imgElement.classList.add("detail-image")
-        ramenMenu.appendChild(imgElement)
+renderRamenMenu();
+addRamenSubmitListener()
+addEditListener()
+
+
+function renderRamenMenu() {
+  //getRamenObjects from API
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((menu) => {
+        for (let ramen in menu) {
+        createRamen(menu[ramen]);
+      }
+      displayRamen(menu[0])
+    });
+}
+
+function createRamen(ramen) {
+  //Renders ramen images in div #ramen-menu
+  const imgElement = document.createElement("img");
+  
+  imgElement.src = ramen.image;
+  imgElement.alt = ramen.name;
+  imgElement.classList.add("detail-image");
+  ramenMenu.appendChild(imgElement);
+  //add event listener to each image in here
+  imgElement.addEventListener("click", () => {
+    displayRamen(ramen);
+  });
+  
+}
+
+function displayRamen(ramen) {
+    displayedRamen = ramen
+  let detailImg = document.getElementById("ramen-detail-image");
+  detailImg.src = ramen.image;
+  detailImg.alt = ramen.name;
+  //   //create h2 title
+  const ramenTitle = document.getElementById("ramen-detail-name");
+  ramenTitle.innerText = ramen.name;
+  //create h3 for restuarant
+  const restaurant = document.getElementById("ramen-detail-restaurant");
+  restaurant.innerText = ramen.restaurant;
+  //append elements into ramen-detail div
+  ramenDetail.appendChild(ramenTitle, restaurant);
+  //display rating
+  let ratingDisplayElement = document.getElementById("rating-display");
+  ratingDisplayElement.innerText = ramen.rating;
+  //display comment
+  let commentDisplayElement = document.getElementById("comment-display");
+  commentDisplayElement.innerText = ramen.comment;
+}
+
+//create new ramen after submitting new-ramen form
+function addRamenSubmitListener() {
+    const newRamenForm = document.getElementById("new-ramen");
+    newRamenForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const newRamenRestaurant = document.getElementById("new-restaurant").value;
+        const newRamenName = document.getElementById("new-name").value;
+        const newRamenImage = document.getElementById("new-image").value;
+        const newRamenRating = document.getElementById("new-rating").value;
+        const newRamenComment = document.getElementById("new-comment").value;
+
+
+        const newRamen = {
+            name: newRamenName,
+            restaurant: newRamenRestaurant,
+            image: newRamenImage,
+            rating: newRamenRating,
+            comment: newRamenComment
+        }
+        createRamen(newRamen)
         
-        
-        //add event listener to each image in here
-        imgElement.addEventListener("click", (e) => {
-            let detailImg = document.getElementById('ramen-detail-image')
-            
-            detailImg.src = e.target.src = ("http://127.0.0.1:5500" + imageUrl.slice(1))
-            detailImg.alt = e.target.alt = String(ramenName)
-            
-            
+//Post: to persist data
+        // fetch(url, {
+        // method: "POST",
+        // headers: {
+        //     "Content-Type": "application/json",
+        //     'Accept': "application/jason",
+        // },
+        // body: JSON.stringify({
+        //     name: newRamenName,
+        //     restaurant: newRamenRestaurant,
+        //     image: newRamenImage,
+        //     rating: newRamenRating,
+        //     comment: newRamenComment,
+        // }),
+        // })
+        // .then((res) => res.json())
+        // .then((data) => createRamen(data));
+    });
+}
 
+function addEditListener(){
+    const editRamenForm = document.getElementById('edit-ramen')
+    editRamenForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const newRating = document.getElementById('edit-rating').value
+        const newComment = document.getElementById('edit-comment').value
+        displayedRamen.rating = newRating;
+        displayedRamen.comment = newComment
+        displayRamen(displayedRamen)
+    })
+    
 
-            //create h2 title
-            const ramenTitle = document.getElementById('ramen-detail-name')
-            ramenTitle.innerText = ramenName
-            //create h3 for restuarant
-            const restaurant = document.getElementById('ramen-detail-restaurant')
-            restaurant.innerText = restaurantName
-            //append elements into ramen-detail div
-            ramenDetail.appendChild(ramenTitle, restaurant, )
-            
-            //display rating
-            let ratingDisplayElement = document.getElementById("rating-display")
-            ratingDisplayElement.innerText = ratingInfo
-
-            //display comment
-            let commentDisplayElement = document.getElementById("comment-display")
-            commentDisplayElement.innerText = ramenComment
-
-        })
-        
-    }
-})
-
-//create new ramen off of form
-//save form element into var to reference
-//prevent default and put event listener on submit form
-//take values from the input field after event is executed
-//create new ramen "card" and populate values based on input
-//append to the menu
-//sorry ran out of time
+}
